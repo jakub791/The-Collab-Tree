bf78a1588c7de0a956edbf996d3f28ae1950260c
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-10-27 18:30:31 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -244,6 +244,7 @@ clickables: {
                 return hasUpgrade(this.layer, 34);
             },
             rewardDescription: "doubles your plot gain, i guess.",
+            currencyDisplayName: "points",
             goal: new Decimal(9001),
 	},
     },
