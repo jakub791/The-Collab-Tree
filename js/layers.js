adb50dcf260f5faaa23821b1b167078dd2cd266d
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-10-28 04:11:35 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -225,6 +225,7 @@ addLayer("s", {
         41: {
             title: "Hatred",
             description() {return "''Impatience Transformation'' is now getting both exponented and multiplied by ^" + format(player.points) + " * " + format(player.points) + "."},
+            currencyLayer: {},
             cost: new Decimal(30),
             unlocked(){
                 return inChallenge(this.layer, 21);
