6b5c8bec5544c973b3bd4e7999bb62585330cfb4
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-10-28 05:08:37 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -144,8 +144,7 @@ addLayer("s", {
                     'height': '200px',
                     'width': '200px',
                     }
-                    else if (!canAffordUpgrade(this.layer, this.id)) {
-                            return {
+                    else if (!canAffordUpgrade(this.layer, this.id))  return {
                             'background-color': '#00353F', 
                             'border-color': '#1a9cb2',
                             'height': '200px',
@@ -254,8 +253,9 @@ addLayer("s", {
                     'border-color': '#451212',
                     'height': '150px',
                     'width': '480px',
-	        },
-	    },
+		    }
+	       },
+          },
     },
     buyables: {
         rows: 1,
