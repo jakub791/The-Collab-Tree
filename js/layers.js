c6bf1f947260b0d4bdd8897867d1280d5ce7427d
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-10-30 20:50:46 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -52,7 +52,7 @@ addLayer("s", {
                 content:
                     [
                     ["display-text",
-                    function() {return player.s.buyables[21] >= 1 ? "You have " + format(player.s.buyables[21]) + " softcap warpers, lowering the ''Impatience'' upgrades costs by " + format(player.s.buyables[21].mul(10)) + "%, increasing plot gain by " + format(new Decimal(2).pow(player.s.buyables[21])) + "x, shenanigans gain by " + format(player.s.buyables[21].mul(new Decimal(2).pow(player.s.buyables[21])).root(2)) + " and weakening softcaps's tetration by " + format(player.s.buyables[21].add(1)) + "/." : ""},
+                    function() {return player.s.buyables[21] >= 1 ? "You have " + format(player.s.buyables[21]) + " softcap warpers, lowering the Impatience upgrades costs by " + format(player.s.buyables[21].mul(10)) + "%, increasing plot gain by " + format(new Decimal(2).pow(player.s.buyables[21])) + "x, shenanigans gain by " + format(player.s.buyables[21].mul(new Decimal(2).pow(player.s.buyables[21])).root(2)) + " and weakening softcaps's tetration by " + format(player.s.buyables[21].add(1)) + "/." : ""},
                     {"color": "dark purple", "font-size": "25px",}],
                     ["blank", "5px"],
                     ["display-text",
@@ -121,7 +121,7 @@ addLayer("s", {
         },
         14: {
             title: "Negotiator.",
-            description: "Unlocks a button, which resets ''Degrading Upgrade.'''s effect.",
+            description: "Unlocks a button, which resets \"Degrading Upgrade.\"'s effect.",
             cost: new Decimal(1800),
             unlocked(){ 
                 if(inChallenge("s", 11)) return false;
@@ -130,7 +130,7 @@ addLayer("s", {
         },
         21: {
             title: "Tiny desk exponent.",
-            description: "Adds ^1.05 to ''Vibing.'' upgrade.",
+            description: "Adds ^1.05 to \"Vibing.\" upgrade.",
             cost: new Decimal(20),
             unlocked(){ 
                 return hasUpgrade(this.layer, 12);
@@ -148,7 +148,7 @@ addLayer("s", {
         },
         22: {
             title: "Supreme Hexagonity.",
-            description: "Unlocks 3 more upgrades, 2 challenges and halves ''But enough grinding, have at you!'''s root effect.",
+            description: "Unlocks 3 more upgrades, 2 challenges and halves \"But enough grinding, have at you!\"'s root effect.",
             cost: new Decimal(999),
             unlocked(){ 
                 return hasUpgrade(this.layer, 21);
@@ -174,7 +174,7 @@ addLayer("s", {
 	    },
         23: {
             title: "Another exponent...?",
-            description: "Adds another ^1.05 to both ''T.D.E.'' and ''Exponent'' upgrades.",
+            description: "Adds another ^1.05 to both \"T.D.E.\" and \"Vibing.\" upgrades.",
             cost: new Decimal(100),
             unlocked(){ 
                 return hasUpgrade(this.layer, 21);
@@ -191,7 +191,7 @@ addLayer("s", {
         },
         31: {
             title: "But enough grinding, have at you!",
-            description: "Exponents most of upgrades around ''S.H.'' based on unspent points.",
+            description: "Exponents most of upgrades around \"S.H.\" based on unspent points.",
             cost: new Decimal(250),
             unlocked() { 
                 let unlockable = true
@@ -312,7 +312,7 @@ addLayer("s", {
             return eff;
         },
         display() {
-        return "''Really? No, seriously, this game is a buggy mess.'' You said. ''How does that dude think I'm gonna reach such a goal without these two ever-expanding upgrades? I swear to god, I WILL break this game, even if that means I'll have to destroy this layer forever.'' Your impatience is getting transformed into anger, exponentionally boosting your plots gain, it's effect and growth by ^"  + format(buyableEffect(this.layer, this.id)) + "."; 
+        return "\"Really? No, seriously, this game is a buggy mess.\" You said. \"How does that dude think I'm gonna reach such a goal without these two ever-expanding upgrades? I swear to god, I WILL bend this game to my will, even if that means I'll have to break the game.\" Your impatience is getting transformed into anger, exponentionally boosting your plots gain, it's effect and growth by ^"  + format(buyableEffect(this.layer, this.id)) + "."; 
 	},
         style() {
             if(player[this.layer].unlocked) return {
@@ -339,7 +339,7 @@ addLayer("s", {
             player.points = new Decimal(0)
         },
         display() { // Everything else displayed in the buyable button after the title
-            return "As you were about to hyperinflate the hell out of this layer, your plot gain suddenly got softcapped tremendously. You've lost your hope, knowing that it's basically impossible to complete this challenge... Until you notice this mythical button. <br> Pressing it will reset your progress. In exchange, you'll be granted with a single Softcap Warper, which decreases ''Impatience'' upgrade's cost, boosts your plot and shenanigans gain and weakens ''The Endgamer'' softcaps. <br> You need to generate " + new Decimal(2).add(player[this.layer].buyables[this.id]) + " plots per second in order to reset.";
+            return "As you were about to hyperinflate the hell out of this layer, your plot gain suddenly got softcapped tremendously. You've lost your hope, knowing that it's basically impossible to complete this challenge... Until you notice this mythical button. <br> Pressing it will reset your progress. In exchange, you'll be granted with a single Softcap Warper, which decreases Impatience upgrades's cost, boosts your plot and shenanigans gain and weakens \"The Endgamer\"'s softcaps. <br> You need to generate " + new Decimal(2).add(player[this.layer].buyables[this.id]) + " plots per second in order to reset.";
 	    },
         style() {
             if(player[this.layer].unlocked) return {
@@ -361,7 +361,7 @@ addLayer("s", {
 	    },
 	},
         31: {
-            title: "Predict unpredicted boredoms.",
+            title: "Predict unpredicted boredom.",
             unlocked() { if (hasChallenge("s", 12)) return false; 
                          else return player[this.layer].unlocked;}, 
             canAfford() { return true },
@@ -394,8 +394,8 @@ addLayer("s", {
             return player.points.max(1).pow(player[this.layer].buyables[this.id].max(1)).mul(player.points.max(1).pow(player[this.layer].buyables[this.id].max(1)))
 	    },
             display() {
-                if (player[this.layer].buyables[41].gte(1)) return "''Impatience Transformation'''s effect will be now exponented by ^(" + format(player.points.max(1).pow(player[this.layer].buyables[this.id].add(1).max(1))) + " x " + format(player.points.max(1).pow(player[this.layer].buyables[this.id].add(1).max(1))) + ") instead. <br> Cost: " + format(layers[this.layer].buyables[this.id].cost()) + " plots."
-                else return "''Impatience Transformation'''s effect will be exponented by ^(" + format(player.points.max(1).pow(player[this.layer].buyables[this.id].add(1).max(1))) + " x " + format(player.points.max(1).pow(player[this.layer].buyables[this.id].add(1).max(1))) + "). <br> <br> Cost: " + format(layers[this.layer].buyables[this.id].cost()) + " plots."
+                if (player[this.layer].buyables[41].gte(1)) return "\"Impatience Transformation\"'s effect will be now exponented by ^(" + format(player.points.max(1).pow(player[this.layer].buyables[this.id].add(1).max(1))) + " x " + format(player.points.max(1).pow(player[this.layer].buyables[this.id].add(1).max(1))) + ") instead. <br> Cost: " + format(layers[this.layer].buyables[this.id].cost()) + " plots."
+                else return "\"Impatience Transformation\"'s effect will be exponented by ^(" + format(player.points.max(1).pow(player[this.layer].buyables[this.id].add(1).max(1))) + " x " + format(player.points.max(1).pow(player[this.layer].buyables[this.id].add(1).max(1))) + "). <br> <br> Cost: " + format(layers[this.layer].buyables[this.id].cost()) + " plots."
                 },
             style() {
                     if (player.points.gte(layers["s"].buyables[41].cost())) return {  
@@ -434,9 +434,9 @@ addLayer("s", {
                 player.points = player.points.sub(layers["s"].buyables[51].cost())
                 player[this.layer].buyables[this.id] = player[this.layer].buyables[this.id].add(1);
             },
-            display() {if (player[this.layer].buyables[61].gte(1) && player[this.layer].buyables[this.id].eq(0)) return "''Impatience Transformation'' boosts all the upgrades (excluding ''Every 60 seconds in real life a minute passes.'', ''Tetrate-inator'' and instead of boosting ''Degrading Upgrade'' like the rest of upgrades, it's multiplier is replaced by ^). <br> <br> Cost: " + format(layers[this.layer].buyables[this.id].cost()) + " plots."
-                       else if (player[this.layer].buyables[this.id].eq(0)) return "''Impatience Transformation'' boosts all the upgrades (excluding ''Every 60 seconds in real life a minute passes.'' and instead of boosting ''Degrading Upgrade'' like the rest of upgrades, it's multiplier is replaced by ^). <br> <br> Cost: " + format(layers[this.layer].buyables[this.id].cost()) + " plots."
-                       if (player[this.layer].buyables[this.id].eq(1)) return "''Degrading Upgrade'''s exponent is replaced with tetration. <nr> Cost: " + format(layers[this.layer].buyables[this.id].cost()) + " plots."
+            display() {if (player[this.layer].buyables[61].gte(1) && player[this.layer].buyables[this.id].eq(0)) return "\"Impatience Transformation\" boosts all the upgrades (excluding \"Every 60 seconds, a minute passes in real life.\", \"Tetrate-inator\" and instead of boosting \"Degrading Upgrade.\" like the rest of upgrades, it's multiplier is replaced by ^). <br> <br> Cost: " + format(layers[this.layer].buyables[this.id].cost()) + " plots."
+                       else if (player[this.layer].buyables[this.id].eq(0)) return "\"Impatience Transformation\" boosts all the upgrades (excluding \"Every 60 seconds, a minute passes in real life.\" and instead of boosting \"Degrading Upgrade.\" like the rest of upgrades, it's multiplier is replaced by ^). <br> <br> Cost: " + format(layers[this.layer].buyables[this.id].cost()) + " plots."
+                       if (player[this.layer].buyables[this.id].eq(1)) return "\"Degrading Upgrade.\"'s exponent is replaced with tetration. <nr> Cost: " + format(layers[this.layer].buyables[this.id].cost()) + " plots."
                        if (player[this.layer].buyables[this.id].eq(2)) return "[LOCKED]"
 		       },
             style() {
@@ -477,8 +477,8 @@ addLayer("s", {
                 player[this.layer].buyables[this.id] = player[this.layer].buyables[this.id].add(1);
             },
             display() {
-                if(player[this.layer].buyables[this.id].eq(0)) return "Nullifies some of second part of ''The Endgamer'', bringing you back two out of three previously removed upgrades. <br> <br> Cost: " + format(layers[this.layer].buyables[this.id].cost()) + " plots."
-                if(player[this.layer].buyables[this.id].eq(1)) return "Nullifies second part of ''The Endgamer'' completely, bringing you back ''Vibing.'' upgrade. <br> <br> Cost: " + format(layers[this.layer].buyables[this.id].cost()) + " plots."
+                if(player[this.layer].buyables[this.id].eq(0)) return "Nullifies some of second part of \"The Endgamer\"'s effect, bringing you back two out of three previously removed upgrades. <br> <br> Cost: " + format(layers[this.layer].buyables[this.id].cost()) + " plots."
+                if(player[this.layer].buyables[this.id].eq(1)) return "Nullifies second part of \"The Endgamer\"'s effect completely, bringing you back \"Vibing.\" upgrade. <br> <br> Cost: " + format(layers[this.layer].buyables[this.id].cost()) + " plots."
                 if(player[this.layer].buyables[this.id].eq(2)) return "\"I've upgraded enough. I'm satisfied.\""
             },
             style() {
@@ -524,8 +524,8 @@ addLayer("s", {
                 player[this.layer].buyables[this.id] = player[this.layer].buyables[this.id].add(1);
             },
             display() {
-                if (player[this.layer].buyables[71].eq(0)) return "You know what? Let's just crank the #%@& up and boost ''Vibing.'' upgrade by 100 times. <br> <br> Cost: " + format(layers[this.layer].buyables[this.id].cost()) + " plots."
-                else return "Boost ''Vibing.'' upgrade by 100 times again. NOW THAT'S A LOT OF MULTIPLIER. <br> <br> Cost: " + format(layers[this.layer].buyables[this.id].cost()) + " plots."
+                if (player[this.layer].buyables[71].eq(0)) return "You know what? Let's just crank the #%@& up and boost \"Vibing.\" upgrade by 100 times. <br> <br> Cost: " + format(layers[this.layer].buyables[this.id].cost()) + " plots."
+                else return "Boost \"Vibing.\" upgrade by 100 times again. NOW THAT'S A LOT OF MULTIPLIER. <br> <br> Cost: " + format(layers[this.layer].buyables[this.id].cost()) + " plots."
             },
             style() {
                     if (player.points.gte(layers["s"].buyables[71].cost())) return {  
@@ -569,8 +569,8 @@ clickables: {
         cols: 2,
         11: {
             name: "Typical Challenge",
-            challengeDescription() {if(hasUpgrade(this.layer, 34)) return "Tetrates your plot gain by 0.5 and removes ''Degrading Upgrade.'' and ''Negotiator.'', but predicted boredoms's first effect is increased to the power of 2.";
-                                   else return "Tetrates your plot gain by 0.5 and removes ''Degrading Upgrade.'' and ''Negotiator.'', but predicted boredoms's effect is increased to the power of 2."
+            challengeDescription() {if(hasUpgrade(this.layer, 34)) return "Tetrates your plot gain by 0.5 and removes \"Degrading Upgrade.\" and \"Negotiator.\", but predicted boredoms's first effect is increased to the power of 2.";
+                                   else return "Tetrates your plot gain by 0.5 and removes \"Degrading Upgrade.\" and \"Negotiator.\", but predicted boredoms's effect is increased to the power of 2."
 	    },
             unlocked() {
                 return hasUpgrade(this.layer, 22);
@@ -582,20 +582,20 @@ clickables: {
 	},
         12: {
             name: "The Reverser",
-            challengeDescription() {if(hasUpgrade(this.layer, 34)) return "Predicted boredoms's first effect is divided by ten and then some (scales up once predicted boredom's first effect reaches 10.01x)."
-                                    else return "Predicted boredoms's first effect is divided by ten and then some (scales up once predicted boredom's effect reaches 10.01x)."
+            challengeDescription() {if(hasUpgrade(this.layer, 34)) return "\"Predicted boredom\"'s first effect is divided by ten and then some (scales up once predicted boredom's first effect reaches 10.01x)."
+                                    else return "Predicted boredom's effect is divided by ten and then some (scales up once predicted boredom's effect reaches 10.01x)."
 	    },
             unlocked() {
                 return hasUpgrade(this.layer, 22);
             },
-            rewardDescription: "''Predict Boredom'' becomes passive, growing up at the rate of 10 clicks per second.",
+            rewardDescription: "You lose the ability to gain \"Predict boredom.\" by yourself and \"Predict boredom.\" becomes passive, growing up at the rate of 10 clicks per second.",
             currencyDisplayName: "plots",
             currencyInternalName: "points",
             goal: new Decimal("50000"),
 	},
         21: {
             name: "The Endgamer",
-            challengeDescription: "Tetrates your plots gain by undecillionth times and removes ''Vibing.'', ''B.E.G,H.A.Y!'' and ''Tetrate-inator.'' upgrades. (Don't even think about bursting through it with hyperinflation, smartass).",
+            challengeDescription: "Tetrates your plots gain by undecillionth times and removes \"Vibing.\", \"B.E.G,H.A.Y!\" and \"Tetrate-inator.\" upgrades. (Don't even think about bursting through it with hyperinflation, smartass).",
             unlocked() {
                 return hasChallenge(this.layer, 11) && hasChallenge(this.layer, 12);
             },
