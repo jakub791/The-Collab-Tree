2f5786f08f55316727a5a8f01e2355cd373256bc
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-10-30 19:44:07 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -12,7 +12,7 @@ addLayer("s", {
 
         baseResource: "plots",
         baseAmount() {return player.points;},
-        requires() {if(inChallenge("s", 11) || inChallenge("s", 12) || inChallenge("s", 21)) return "Infinity"
+        requires() {if(inChallenge("s", 11) || inChallenge("s", 12) || inChallenge("s", 21)) return new Decimal(Infinity)
                     else return new Decimal(1)},
         type: "normal",
         exponent: 0.5,
