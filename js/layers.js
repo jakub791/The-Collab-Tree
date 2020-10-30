2b9c73839445e28675b4ec770bacab30c057fb5b
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-10-31 01:52:11 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -469,7 +469,7 @@ addLayer("s", {
             currencyDisplayName: "plots",
             currencyInternalName: "points",
             cost(x=player[this.layer].buyables[this.id]) { 
-                let cost = new Decimal(10).sub(player[this.layer].buyables[21]).mul(new Decimal(5).pow(x))
+                let cost = new Decimal(10).sub(player[this.layer].buyables[21]).mul(new Decimal(2).pow(x))
                 return cost
             },
             buy() {
