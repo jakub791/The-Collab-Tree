3aba7f2d19be6b445ef861ea635895c6892828fa
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-10-30 18:19:09 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -383,7 +383,7 @@ addLayer("s", {
             currencyDisplayName: "plots",
             currencyInternalName: "points",
             cost(x=player[this.layer].buyables[this.id]) { 
-                let cost = new Decimal(10).mul(new Decimal(2).pow(x))
+                let cost = new Decimal(10).sub(player[this.layer].buyables[21]).mul(new Decimal(2).pow(x))
                 return cost
             },
             buy() {
@@ -427,7 +427,7 @@ addLayer("s", {
             currencyDisplayName: "plots",
             currencyInternalName: "points",
             cost(x=player[this.layer].buyables[this.id]) { 
-                let cost = new Decimal(10).mul(new Decimal(3).pow(x))
+                let cost = new Decimal(10).sub(player[this.layer].buyables[21]).mul(new Decimal(3).pow(x))
                 return cost
             },
             buy() {
@@ -469,7 +469,7 @@ addLayer("s", {
             currencyDisplayName: "plots",
             currencyInternalName: "points",
             cost(x=player[this.layer].buyables[this.id]) { 
-                let cost = new Decimal(10).mul(new Decimal(5).pow(x))
+                let cost = new Decimal(10).sub(player[this.layer].buyables[21]).mul(new Decimal(5).pow(x))
                 return cost
             },
             buy() {
@@ -513,7 +513,7 @@ addLayer("s", {
             currencyDisplayName: "plots",
             currencyInternalName: "points",
             cost(x=player[this.layer].buyables[this.id]) { 
-                let cost = new Decimal(10).mul(new Decimal(7).pow(x))
+                let cost = new Decimal(10).sub(player[this.layer].buyables[21]).mul(new Decimal(7).pow(x))
                 return cost
             },
             effect() {
