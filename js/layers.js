31404a1dd9d020206937a1b6f477fb2ae5679e32
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-10-31 14:35:26 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -11,9 +11,10 @@ addLayer("s", {
         row: 0,
         resetDescription: "Obliterate your plots for ",
         baseResource: "plots",
-        baseAmount() {if (inChallenge("s", 11) || inChallenge("s", 12) || inChallenge("s", 21)) return new Decimal(Infinity)
-                      else return player.points},
-        requires: new Decimal(1),
+        baseAmount() {return player.points},
+        requires() { if (inChallenge("s", 11) || inChallenge("s", 12) || inChallenge("s", 21)) return new Decimal(Infinity)
+                     else return new Decimal(1)
+                   },
         type: "normal",
         exponent: 0.5,
 
