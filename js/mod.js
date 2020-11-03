ef0de5c4dfaca9f24538599ea5fefda262ac2160
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-11-03 17:44:41 +0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -13,7 +13,7 @@ let modInfo = {
 // Set your version in num and name
 let VERSION = {
 	num: "0.35",
-	name: "Transcendence Update.",
+	name: "Butter Biscuit Update... Wait, what?",
 }
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
