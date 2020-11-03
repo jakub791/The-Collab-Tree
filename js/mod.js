6b3971cf780c594a727db57a9ef28ca1c5636062
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-11-03 17:31:39 +0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -12,8 +12,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.333",
-	name: "Rework Update.",
+	num: "0.35",
+	name: "Transcendence Update.",
 }
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
@@ -50,35 +50,41 @@ function getPointGen() {
         if (hasUpgrade("s", 13) && player.s.buyables[51].eq(2) && inChallenge("s", 21)) gain = gain.tetrate(upgradeEffect("s", 13));
         else if (hasUpgrade("s", 13) && player.s.buyables[51].eq(1) && inChallenge("s", 21)) gain = gain.pow(upgradeEffect("s", 13));
         else if (hasUpgrade("s", 13)) gain = gain.times(upgradeEffect("s", 13));
+		if (hasUpgrade("c", 21)) gain = gain.mul(new Decimal(1).add(player.c.chaoticEnergy.div(10)).root(10))
         if (inChallenge("s", 11)) gain = gain.tetrate(new Decimal(0.5));
         if (inChallenge("s", 21)) gain = gain.tetrate(new Decimal(0.000000000000000000000000000000000001)).pow(buyableEffect("s", 12))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(2))) gain = new Decimal(1).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(4096).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(3))) gain = new Decimal(2).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(13824).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(4))) gain = new Decimal(3).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(65536).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(5))) gain = new Decimal(4).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(400000).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(6))) gain = new Decimal(5).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(2985984).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(7))) gain = new Decimal(6).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(26353376).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(8))) gain = new Decimal(7).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(268435456).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(9))) gain = new Decimal(8).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(3099363912).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(10))) gain = new Decimal(9).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(40000000000).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(11))) gain = new Decimal(10).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(570623341222).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(12))) gain = new Decimal(11).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(8916100448256).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(13))) gain = new Decimal(12).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(151437553296126.5).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(14))) gain = new Decimal(13).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(2778001706389504).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(15))) gain = new Decimal(14).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(54736736297607421.875).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(16))) gain = new Decimal(15).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(1152921504606846976).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(17))) gain = new Decimal(16).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(25851258183948023880.53125).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(18))) gain = new Decimal(17).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(614787626176508399616).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(19))) gain = new Decimal(18).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(15456403559846199915031.0859375).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(20))) gain = new Decimal(19).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(409600000000000000000000).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(21))) gain = new Decimal(20).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(11411302770285122112072508.634766).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(22))) gain = new Decimal(21).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(333425661488495661520162816).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(23))) gain = new Decimal(22).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(10195541015550738298024918413.363).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(24))) gain = new Decimal(23).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(325619086145088897570576531456).max(1))
-        if (inChallenge("s", 21) && gain.gt(new Decimal(25))) gain = new Decimal(24).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(10842021724855044340074528008699).max(1))
-        if (inChallenge("s", 21) && gain.gte(new Decimal(26))) gain = new Decimal(25).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(375739720471628253832804827172860).max(1))
-        if (inChallenge("s", 21) && hasMilestone("c", 2)) gain = gain.add(0.5)
-        if (inChallenge("s", 21) && gain.gte(new Decimal(100))) gain = new Decimal(100)
+        if (inChallenge("s", 21)) { 
+        if (gain.gt(new Decimal(2))) gain = new Decimal(1).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(4096).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(3))) gain = new Decimal(2).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(13824).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(4))) gain = new Decimal(3).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(65536).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(5))) gain = new Decimal(4).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(400000).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(6))) gain = new Decimal(5).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(2985984).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(7))) gain = new Decimal(6).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(26353376).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(8))) gain = new Decimal(7).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(268435456).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(9))) gain = new Decimal(8).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(3099363912).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(10))) gain = new Decimal(9).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(40000000000).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(11))) gain = new Decimal(10).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(570623341222).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(12))) gain = new Decimal(11).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(8916100448256).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(13))) gain = new Decimal(12).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(151437553296126.5).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(14))) gain = new Decimal(13).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(2778001706389504).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(15))) gain = new Decimal(14).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(54736736297607421.875).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(16))) gain = new Decimal(15).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(1152921504606846976).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(17))) gain = new Decimal(16).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(25851258183948023880.53125).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(18))) gain = new Decimal(17).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(614787626176508399616).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(19))) gain = new Decimal(18).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(15456403559846199915031.0859375).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(20))) gain = new Decimal(19).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(409600000000000000000000).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(21))) gain = new Decimal(20).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(11411302770285122112072508.634766)).max(1))
+        if (gain.gt(new Decimal(22))) gain = new Decimal(21).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(333425661488495661520162816).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(23))) gain = new Decimal(22).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(10195541015550738298024918413.363).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(24))) gain = new Decimal(23).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(325619086145088897570576531456).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(25))) gain = new Decimal(24).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(10842021724855044340074528008699).div(layers.t.effect())).max(1))
+        if (gain.gt(new Decimal(26))) gain = new Decimal(25).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(375739720471628253832804827172860).div(layers.t.effect())).max(1))
+		if (gain.gt(new Decimal(27))) gain = new Decimal(26).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(13532302497651299131721485309422000).div(layers.t.effect())).max(1))
+		if (gain.gt(new Decimal(28))) gain = new Decimal(27).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(505760545551351545144237186483220000).div(layers.t.effect())).max(1))
+		if (gain.gt(new Decimal(29))) gain = new Decimal(28).add(gain.log(new Decimal(2).pow(hahaSoftcapGoBrrrrrrrr)).root(new Decimal(19589890694894494129652620046471000000).div(layers.t.effect())).max(1))
+        if (hasMilestone("c", 2)) gain = gain.add(0.5)
+        if (gain.gte(new Decimal(30))) gain = new Decimal(30)
+		}
 	return gain
 }
 
