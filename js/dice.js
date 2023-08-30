544caca2c9835c0d848572bbac8ebca89d113b68
unsoftcapped3 <75136164+unsoftcapped3@users.noreply.github.com>
2023-08-30 22:21:22 +0000

    DO NOT CHANGE ANYTHING ELSE WITHOUT PULLING THIS COMMIT FIRST

--- js/dice.js
+++ js/dice.js
@@ -3,7 +3,7 @@ addLayer("tdr", {
     symbol() {
         return `${player.tdr.points.toNumber()}d${tmp.tdr.effect}`;
     }, // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 2, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() {
         return {
             unlocked: true,
@@ -30,7 +30,7 @@ addLayer("tdr", {
     exponent: Decimal.dOne,
     gainMult: Decimal.dOne,
     gainExp: Decimal.dOne,
-    row: 0,
+    row: 1,
     hotkeys: [
         {
             key: "d",
