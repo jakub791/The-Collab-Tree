555d3886797710852eccf1c223d6150b07f628e6
TheCamelliaTree <92651830+TheCamelliaTree@users.noreply.github.com>
2023-08-30 23:15:47 +0000

    adding milestones to Lycoris Layer

--- js/eden.js
+++ js/eden.js
@@ -1,22 +1,46 @@
 addLayer("e", {
+    name: "Lycoris",
+    symbol: "L",
     startData() {
         return {
-            unlocked: true,
+            unlocked: false,
             points: Decimal.dZero
         };
     },
     color: "#E0E1CC",
-    resource: "prestige points",
+    resource: "Lycoris Flowers",
     row: 1,
-
-    baseResource: "points",
+    baseResource: "Dice Roll Points",
     baseAmount() {
-        return player.points;
+        return player.tdr.totalRoll;
     },
-    requires: Decimal.dTen,
-    type: "normal",
+    requires: new Decimal(222),
+    type: "static",
     exponent: 0.5,
     gainMult: Decimal.dOne,
     gainExp: Decimal.dOne,
-    layerShown: true
+    layerShown() { return player.tdr.points.gte(5) || player.e.unlocked},
+    tabFormat: {
+        milestones: {
+            unlocked: true,
+            content: [
+                ["display-text", () => `You have planted ${player.e.points} Lycoris Flowers, in the Amnehilesie of Eden.`],
+                "prestige-button",
+                "blank",
+                "milestones"
+            ]
+        }
+    },
+    milestones: {
+        0: {
+            requirementDescription: "Welcome to the Amnehilesie of Eden, Esteemed Guests. (1 total Lycoris Flower Planted)",
+            effectDescription: "Oh, You only started planting these flowers? Don't worry, our butlers will give you a 5x boost to point gain to help you plant these faster.",
+            done() { return player.e.total.gte(1)}
+        },
+        1: {
+            requirementDescription: "These flowers aren't going to plant themselves... (2 Total Lycoris Flowers planted)",
+            effectDescription: "Maybe you'll need some more help. Don't worry, we will support you with upgrades and passive gain to your previous endenavors, although, passive gain will have to be at a minimum since this is still experimental for us. Only 1%!",
+            done() { return player.e.total.gte(2)}
+        },
+    },
 });
