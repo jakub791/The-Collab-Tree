3233a09be330cddeccb65633884f2c6f9373455f
z3n1th4l@gmail.com <z3n1th4l@gmail.com>
2023-08-30 18:40:19 +0300

    whar

--- js/h.js
+++ js/h.js
@@ -202,7 +202,7 @@ addLayer("t", {
           'background-size': "180%"
         }
       },
-      tooltip: `you got super-scaled`
+      tooltip: `you got super-scaled lmafo`
     }
   },
   tabFormat: {
