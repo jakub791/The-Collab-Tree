59e306b8aa5441e8e78ceb6b98582f147a44b858
Icecreamdudes <Icecreamdudes@users.noreply.github.com>
2023-08-31 02:57:21 +0000

    Prettified Code!

--- js/je.js
+++ js/je.js
@@ -22,7 +22,7 @@ addLayer("je", {
     return player.points;
   }, // Get the current amount of baseResource
   type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-  branches: ["cv", "tb"], 
+  branches: ["cv", "tb"],
   exponent: 0.44, // Prestige currency exponent
   gainMult() {
     // Calculate the multiplier for main currency from bonuses
@@ -33,10 +33,9 @@ addLayer("je", {
     // Calculate the exponent on main currency from bonuses
     return new Decimal(1);
   },
-  update(delta)
-  {
+  update(delta) {
     if (player.jacorbscene.eq(20)) {
-      player.jacorbcutscene = new Decimal(0)
+      player.jacorbcutscene = new Decimal(0);
     }
   },
   hotkeys: [
@@ -50,37 +49,53 @@ addLayer("je", {
   ],
   clickables: {
     11: {
-        title() { return "<img src='resources/forwardarrow.png'style='width:calc(80%);height:calc(80%);margin:10%'></img>" },
-        canClick() { return player.jacorbcutscene.eq(1) },
-        unlocked() { return player.jacorbscene.lt(20) },
-        onClick() {
-            player.jacorbscene = player.jacorbscene.add(1)
-        },
+      title() {
+        return "<img src='resources/forwardarrow.png'style='width:calc(80%);height:calc(80%);margin:10%'></img>";
+      },
+      canClick() {
+        return player.jacorbcutscene.eq(1);
+      },
+      unlocked() {
+        return player.jacorbscene.lt(20);
+      },
+      onClick() {
+        player.jacorbscene = player.jacorbscene.add(1);
+      },
     },
     12: {
-        title() { return "<img src='resources/backarrow.png'style='width:calc(80%);height:calc(80%);margin:10%'></img>" },
-        canClick() { return player.jacorbcutscene.eq(1) },
-        unlocked() { return player.jacorbscene.lt(20) && player.jacorbscene.neq(0) },
-        onClick() {
-            player.jacorbscene = player.jacorbscene.sub(1)
-        },
+      title() {
+        return "<img src='resources/backarrow.png'style='width:calc(80%);height:calc(80%);margin:10%'></img>";
+      },
+      canClick() {
+        return player.jacorbcutscene.eq(1);
+      },
+      unlocked() {
+        return player.jacorbscene.lt(20) && player.jacorbscene.neq(0);
+      },
+      onClick() {
+        player.jacorbscene = player.jacorbscene.sub(1);
+      },
     },
   },
   upgrades: {
-    11:
-    {
-        title: "Jacorb Upgrade I",
-        unlocked() { return player.jacorbcutscene.eq(0) },
-        description: "Boosts coronavirus gain based on jacorbian energy.",
-        cost: new Decimal(10),
-        currencyLocation() { return player.je },
-        effect() 
-        {
-             return player[this.layer].points.add(1).pow(0.8)
-        },
-        effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
-        currencyDisplayName: "Jacorbian Energy",
-        currencyInternalName: "points",
+    11: {
+      title: "Jacorb Upgrade I",
+      unlocked() {
+        return player.jacorbcutscene.eq(0);
+      },
+      description: "Boosts coronavirus gain based on jacorbian energy.",
+      cost: new Decimal(10),
+      currencyLocation() {
+        return player.je;
+      },
+      effect() {
+        return player[this.layer].points.add(1).pow(0.8);
+      },
+      effectDisplay() {
+        return format(upgradeEffect(this.layer, this.id)) + "x";
+      }, // Add formatting to the effect
+      currencyDisplayName: "Jacorbian Energy",
+      currencyInternalName: "points",
     },
   },
   tabFormat: [
@@ -94,28 +109,187 @@ addLayer("je", {
       "main-display",
       "prestige-button",
       ["blank", "25px"],
-      ["raw-html", function () { return player.jacorbscene.eq(1) ? "<h1>Hi. My name is Jacorb. I'm the creator of The Prestige Tree." : "" }, { "color": "purple", "font-size": "12px"}],
-      ["raw-html", function () { return player.jacorbscene.eq(2) ? "<h1>I'm giving you access to my energy. Make sure to use it well." : "" }, { "color": "purple", "font-size": "12px"}],
-      ["raw-html", function () { return player.jacorbscene.eq(3) ? "<h1>The modding tree sure is interesting. It is very great." : "" }, { "color": "purple", "font-size": "12px"}],
-      ["raw-html", function () { return player.jacorbscene.eq(4) ? "<h1>A whole community was built around making these mods." : "" }, { "color": "purple", "font-size": "12px"}],
-      ["raw-html", function () { return player.jacorbscene.eq(5) ? "<h1>A lot of people were able to make their dream games." : "" }, { "color": "purple", "font-size": "12px"}],
-      ["raw-html", function () { return player.jacorbscene.eq(6) ? "<h1>However, I am in exile and need to be freed." : "" }, { "color": "purple", "font-size": "12px"}],
-      ["raw-html", function () { return player.jacorbscene.eq(7) ? "<h1>Thankfully, I have someone doing that for me now." : "" }, { "color": "purple", "font-size": "12px"}],
-      ["raw-html", function () { return player.jacorbscene.eq(8) ? "<h1>But your objective is to get through this tree." : "" }, { "color": "purple", "font-size": "12px"}],
-      ["raw-html", function () { return player.jacorbscene.eq(9) ? "<h1>The final layer will contain an object of great power." : "" }, { "color": "purple", "font-size": "12px"}],
-      ["raw-html", function () { return player.jacorbscene.eq(10) ? "<h1>Legends call it THE MODDING STONE." : "" }, { "color": "purple", "font-size": "12px"}],
-      ["raw-html", function () { return player.jacorbscene.eq(11) ? "<h1>This stone wields power of many mods." : "" }, { "color": "purple", "font-size": "12px"}],
-      ["raw-html", function () { return player.jacorbscene.eq(12) ? "<h1>It also has the power to contain mods." : "" }, { "color": "purple", "font-size": "12px"}],
-      ["raw-html", function () { return player.jacorbscene.eq(13) ? "<h1>That is why I need you to find it." : "" }, { "color": "purple", "font-size": "12px"}],
-      ["raw-html", function () { return player.jacorbscene.eq(14) ? "<h1>So that once the prestige tree is rebuilt," : "" }, { "color": "purple", "font-size": "12px"}],
-      ["raw-html", function () { return player.jacorbscene.eq(15) ? "<h1>It cannot be destroyed." : "" }, { "color": "purple", "font-size": "12px"}],
-      ["raw-html", function () { return player.jacorbscene.eq(16) ? "<h1>I'm just here to help." : "" }, { "color": "purple", "font-size": "12px"}],
-      ["raw-html", function () { return player.jacorbscene.eq(17) ? "<h1>Gotta pay you back for the help you're giving me." : "" }, { "color": "purple", "font-size": "12px"}],
-      ["raw-html", function () { return player.jacorbscene.eq(18) ? "<h1>Everyone here has been working hard on their layers." : "" }, { "color": "purple", "font-size": "12px"}],
-      ["raw-html", function () { return player.jacorbscene.eq(19) ? "<h1>There will be a lot more to come..." : "" }, { "color": "purple", "font-size": "12px"}],
+      [
+        "raw-html",
+        function () {
+          return player.jacorbscene.eq(1)
+            ? "<h1>Hi. My name is Jacorb. I'm the creator of The Prestige Tree."
+            : "";
+        },
+        { color: "purple", "font-size": "12px" },
+      ],
+      [
+        "raw-html",
+        function () {
+          return player.jacorbscene.eq(2)
+            ? "<h1>I'm giving you access to my energy. Make sure to use it well."
+            : "";
+        },
+        { color: "purple", "font-size": "12px" },
+      ],
+      [
+        "raw-html",
+        function () {
+          return player.jacorbscene.eq(3)
+            ? "<h1>The modding tree sure is interesting. It is very great."
+            : "";
+        },
+        { color: "purple", "font-size": "12px" },
+      ],
+      [
+        "raw-html",
+        function () {
+          return player.jacorbscene.eq(4)
+            ? "<h1>A whole community was built around making these mods."
+            : "";
+        },
+        { color: "purple", "font-size": "12px" },
+      ],
+      [
+        "raw-html",
+        function () {
+          return player.jacorbscene.eq(5)
+            ? "<h1>A lot of people were able to make their dream games."
+            : "";
+        },
+        { color: "purple", "font-size": "12px" },
+      ],
+      [
+        "raw-html",
+        function () {
+          return player.jacorbscene.eq(6)
+            ? "<h1>However, I am in exile and need to be freed."
+            : "";
+        },
+        { color: "purple", "font-size": "12px" },
+      ],
+      [
+        "raw-html",
+        function () {
+          return player.jacorbscene.eq(7)
+            ? "<h1>Thankfully, I have someone doing that for me now."
+            : "";
+        },
+        { color: "purple", "font-size": "12px" },
+      ],
+      [
+        "raw-html",
+        function () {
+          return player.jacorbscene.eq(8)
+            ? "<h1>But your objective is to get through this tree."
+            : "";
+        },
+        { color: "purple", "font-size": "12px" },
+      ],
+      [
+        "raw-html",
+        function () {
+          return player.jacorbscene.eq(9)
+            ? "<h1>The final layer will contain an object of great power."
+            : "";
+        },
+        { color: "purple", "font-size": "12px" },
+      ],
+      [
+        "raw-html",
+        function () {
+          return player.jacorbscene.eq(10)
+            ? "<h1>Legends call it THE MODDING STONE."
+            : "";
+        },
+        { color: "purple", "font-size": "12px" },
+      ],
+      [
+        "raw-html",
+        function () {
+          return player.jacorbscene.eq(11)
+            ? "<h1>This stone wields power of many mods."
+            : "";
+        },
+        { color: "purple", "font-size": "12px" },
+      ],
+      [
+        "raw-html",
+        function () {
+          return player.jacorbscene.eq(12)
+            ? "<h1>It also has the power to contain mods."
+            : "";
+        },
+        { color: "purple", "font-size": "12px" },
+      ],
+      [
+        "raw-html",
+        function () {
+          return player.jacorbscene.eq(13)
+            ? "<h1>That is why I need you to find it."
+            : "";
+        },
+        { color: "purple", "font-size": "12px" },
+      ],
+      [
+        "raw-html",
+        function () {
+          return player.jacorbscene.eq(14)
+            ? "<h1>So that once the prestige tree is rebuilt,"
+            : "";
+        },
+        { color: "purple", "font-size": "12px" },
+      ],
+      [
+        "raw-html",
+        function () {
+          return player.jacorbscene.eq(15) ? "<h1>It cannot be destroyed." : "";
+        },
+        { color: "purple", "font-size": "12px" },
+      ],
+      [
+        "raw-html",
+        function () {
+          return player.jacorbscene.eq(16) ? "<h1>I'm just here to help." : "";
+        },
+        { color: "purple", "font-size": "12px" },
+      ],
+      [
+        "raw-html",
+        function () {
+          return player.jacorbscene.eq(17)
+            ? "<h1>Gotta pay you back for the help you're giving me."
+            : "";
+        },
+        { color: "purple", "font-size": "12px" },
+      ],
+      [
+        "raw-html",
+        function () {
+          return player.jacorbscene.eq(18)
+            ? "<h1>Everyone here has been working hard on their layers."
+            : "";
+        },
+        { color: "purple", "font-size": "12px" },
+      ],
+      [
+        "raw-html",
+        function () {
+          return player.jacorbscene.eq(19)
+            ? "<h1>There will be a lot more to come..."
+            : "";
+        },
+        { color: "purple", "font-size": "12px" },
+      ],
       ["blank", "25px"],
-      ["row", [["clickable", 12], ["clickable", 11]]],
-      ["raw-html", function () { return "<audio controls autoplay loop hidden><source src=music/jacorbtab.mp3 type<=audio/mp3>loop=true hidden=true autostart=true</audio>"  }],
+      [
+        "row",
+        [
+          ["clickable", 12],
+          ["clickable", 11],
+        ],
+      ],
+      [
+        "raw-html",
+        function () {
+          return "<audio controls autoplay loop hidden><source src=music/jacorbtab.mp3 type<=audio/mp3>loop=true hidden=true autostart=true</audio>";
+        },
+      ],
       ["row", [["upgrade", 11]]],
     ],
   ],
