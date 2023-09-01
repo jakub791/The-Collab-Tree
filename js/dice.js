bea50955e1cbfa9bbee4a346a0f4c5f01a5d7b03
Icecreamdudes <Icecreamdudes@users.noreply.github.com>
2023-09-01 02:49:10 +0000

    Prettified Code!

--- js/dice.js
+++ js/dice.js
@@ -216,9 +216,8 @@ addLayer("tdr", {
         diff *
         (hasUpgrade("tb", 15)
           ? tmp.t.timeCalculation.add(10).log10().toNumber()
-          : 1) * (hasUpgrade("je", 13)
-          ? upgradeEffect("je", 13)
-          : 1);
+          : 1) *
+        (hasUpgrade("je", 13) ? upgradeEffect("je", 13) : 1);
     }
     player.tdr.cooldown = Math.max(player.tdr.cooldown, 0);
   },
