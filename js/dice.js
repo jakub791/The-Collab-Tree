826a86350a4d617dc3c4215408806ddce72db715
unsoftcapped3 <75136164+unsoftcapped3@users.noreply.github.com>
2023-09-03 21:18:06 +0000

    add challenge effect to base cooldown display

--- js/dice.js
+++ js/dice.js
@@ -113,7 +113,7 @@ addLayer("tdr", {
         player.tdr.cooldown = cool;
       },
       display() {
-        return `Roll your dice. <span style="color: red">WARNING: THE BASE COOLDOWN IS <b>24</b> HOURS.</span>
+        return `Roll your dice. <span style="color: red">WARNING: THE BASE COOLDOWN IS <b>${hasChallenge(this.layer,11)?24-player.e.points.min(20).floor():24}</b> HOURS.</span>
                 Cooldown: ${formatTime(player.tdr.cooldown)}`;
       },
     },
